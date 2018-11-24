16832fd5f35d0ffe41fbbf9fa0e0e46b27b9c7f8
YJQ@Nvidia1080Ti <littlepretty881203@gmail.com>
2018-11-23 21:17:31 -0600

    Script plots P/R/F1 scores using run results
    Signed-off-by: YJQ@Nvidia1080Ti <littlepretty881203@gmail.com>

---
+++ maldefender/plot_prf_scores.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3.7
+
+from matplotlib import pyplot as plt
+import matplotlib
+import pandas as pd
+import glog as log
+
+font = {'family': 'normal', 'size': 28}
+matplotlib.rc('font', **font)
+matplotlib.rcParams["figure.figsize"] = [14, 10]
+
+
+def plotPrfScores(data, scorePath):
+    df = pd.read_csv(scorePath, sep=',')
+    log.info('Latex table for scores:\n' + df.to_latex(index=False))
+    print(df)
+    log.debug(df['Precision'].size)
+    x = range(0, df['Precision'].size)
+
+    plt.plot(x, df['Precision'], 'r', ls=':', marker='+',
+             linewidth=2, markersize=12.0, markeredgewidth=3,
+             label='Precision')
+    plt.plot(x, df['Recall'], 'g', ls='--', marker='x',
+             linewidth=2, markersize=12.0, markeredgewidth=3,
+             label='Recall')
+    plt.plot(x, df['F1'], 'b', ls='-.', marker='*',
+             linewidth=2, markersize=12.0, markeredgewidth=3,
+             label='F1')
+    plt.xticks(x, df['Family'], rotation=45)
+    plt.xlim((0, len(x) - 1))
+    plt.ylabel('Performance Scores')
+    minEach = list(df.min(numeric_only=True))
+    yBottom = int(min(minEach) * 10) / 10.0
+    plt.ylim((yBottom, 1.0))
+    plt.grid(which='both', axis='both', ls='-.')
+    plt.legend()
+    leftDist = 0.10 if data == 'YanAcfg' else 0.14
+    plt.subplots_adjust(left=leftDist, bottom=0.16, right=0.96, top=0.97)
+    plt.savefig('%sScores.pdf' % data, format='pdf')
+    log.info(f'Figure for {data} saved to {data}sScores.pdf')
+
+
+if __name__ == '__main__':
+    scorePaths = {
+        'MsAcfg': '../../MSACFG/Nov16TrainValidScores/MSACFG_valid_pr_scores.csv',
+        'YanAcfg': '../../YANACFG/Nov15TrainValidScores/YANACFG_valid_pr_scores.txt',
+    }
+    for (name, path) in scorePaths.items():
+        plotPrfScores(name, path)
