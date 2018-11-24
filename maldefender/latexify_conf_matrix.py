476c68153e05d8c457c934c555b337dcc341725f
YJQ@Nvidia1080Ti <littlepretty881203@gmail.com>
2018-11-23 21:15:52 -0600

    Script convers confusion matrix into latex tables
    Signed-off-by: YJQ@Nvidia1080Ti <littlepretty881203@gmail.com>

---
+++ maldefender/latexify_conf_matrix.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3.7
+
+import pandas as pd
+import glog as log
+
+
+def latexifyConfMatrix(scorePath, familyNames):
+    df = pd.read_csv(scorePath, delim_whitespace=True,
+                     names=familyNames)
+    df.insert(0, 'Family', familyNames)
+    log.info('Latex table for confusion matrix:\n' + df.to_latex(index=False))
+
+
+if __name__ == '__main__':
+    scorePaths = [
+        '../../MSACFG/Nov16TrainValidScores/MSACFG_valid_confusion_matrix.txt',
+        '../../YANACFG/Nov15TrainValidScores/YANACFG_valid_confusion_matrix.txt',
+    ]
+    families = [
+        ['Ramnit', 'Lollipop', 'KeliVer3', 'Vundo', 'Simda', 'Tracur',
+         'KeliVer1', 'Obf.ACY', 'Gatak'],
+        ['Bagle', 'Benign', 'Bifrose', 'Hupigon', 'Koobface', 'Ldpinch',
+         'Lmir', 'Rbot', 'Sdbot', 'Swizzor', 'Vundo', 'Zbot', 'Zlob']
+    ]
+    for (sp, name) in zip(scorePaths, families):
+        latexifyConfMatrix(sp, name)
